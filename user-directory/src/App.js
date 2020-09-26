30e09052d3b8f3db94f28ae195a98286cad37359
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-09-25 20:29:18 -0400

    redid and completed Navbar component

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,32 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Child from "./components/Child";
 import "./App.css";
-
-const postArray = [1, 2, 3, 4, 5];
+import NavBar from "./components/NavBar/index";
 
 function App() {
-  const [userName, setUserName] = useState("Reginald");
-
-  useEffect(() => {
-    console.log(userName);
-  }, [userName]);
-
-  const handleChange = (e) => {
-    let { value } = e.target;
-    setUserName(value);
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Child name={userName} setUserName={setUserName}></Child> */}
-        {postArray.map((post) => (
-          <Child name={post} key={post} />
-        ))}
-        <h2>{userName}</h2>
-        <input type="text" onChange={handleChange} value={userName}></input>
-      </header>
-    </div>
-  );
+  return <NavBar />;
 }
 
 export default App;
